8d563c1587a0ae7aa423449500689fca43162085
Nic Mitoulis <120728902+nmitou@users.noreply.github.com>
2023-03-08 16:43:08 +0200

    remove func definition to use data utils func

--- examples/csv_tests.cpp
+++ examples/csv_tests.cpp
@@ -1,81 +1,29 @@
+/*
+	To compile:
+		Either:
+		- first compile utils.cpp in root folder to object file utils.o
+			g++ -I path\to\eigen -c utils.cpp
+		- then compile with
+			g++ -I path\to\eigen -o csv_tests path\to\utilsobject\utils.o csv_tests.cpp
+		Or:
+		- compile and link all at once
+			g++ -I path\to\eigen -o csv_tests path\to\utilssource\utils.cpp csv_tests.cpp
+*/
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <exception>
-
-#include <vector>
-#include <string>
 #include <Eigen/Dense>
-
-Eigen::ArrayXXd CSVToEigenArr(std::string csvPath) ;
+#include "../utils.h"
 
 int main() {
 
-	Eigen::ArrayXXd spotData1 = CSVToEigenArr("example.csv");
-	Eigen::ArrayXXd spotData2 = CSVToEigenArr("example_commas_nodates.csv");
-	Eigen::ArrayXXd spotData3 = CSVToEigenArr("example_nodates.csv");
+	Eigen::ArrayXXd spotData1 = data_utils::CSVToEigenArray("example.csv");
+	Eigen::ArrayXXd spotData2 = data_utils::CSVToEigenArray("example_commas_nodates.csv");
+	Eigen::ArrayXXd spotData3 = data_utils::CSVToEigenArray("example_nodates.csv");
 
 	std::cout << spotData1 << std::endl << '\n';
 	std::cout << spotData2 << std::endl << '\n';
 	std::cout << spotData3 << std::endl << '\n';
 
+	// Eigen::ArrayXXd spotData4 = data_utils::CSVToEigenArray("does_not_exist.csv");
+
 	return 0;
-}
-
-Eigen::ArrayXXd CSVToEigenArr(std::string csvPath) {
-	std::ifstream csv(csvPath);
-	if(!csv) {
-		std::cerr << "File could not be opened." << std::endl;
-		std::cerr << "Error code: " << std::strerror(errno) << std::endl;
-		csv.close();
-		// throw;
-		std::terminate(); // as yet, unsure if this is really bad
-	}
-
-	/*
-		Assume csv could have two columns of date, spot prices
-		or just one column of spot prices. Need to handle both
-		cases.
-		Extracting dates for future use/changes if necessary.
-	*/
-
-	std::vector<std::string> dates;
-	std::vector<double> spots;
-
-	// get first line to check if there's a comma
-	std::string line;
-	std::getline(csv, line);
-	unsigned int nRows = 1;
-	
-	int commaPos = line.find(",");
-
-	if(commaPos != std::string::npos) { // then csv has dates and spot prices columns
-		// extract the first line
-		std::stringstream lineStream(line);
-		std::string cell;
-		std::getline(lineStream, cell, ',');
-		dates.push_back(cell);
-		std::getline(lineStream, cell);
-		spots.push_back(std::stod(cell));
-		// do the rest of the lines
-		while(std::getline(csv, line)) {
-			std::stringstream lineStream(line);
-			std::getline(lineStream, cell, ',');
-			dates.push_back(cell);
-			std::getline(lineStream, cell);
-			spots.push_back(std::stod(cell));
-			nRows++;
-		}
-	} else { // then csv only has spot price column
-		// again, extract the first line
-		spots.push_back(std::stod(line));
-		// do the remaining lines
-		while(std::getline(csv, line)) {
-			spots.push_back(std::stod(line));
-			nRows++;
-		}
-	}
-
-	csv.close();
-	return Eigen::Map<Eigen::ArrayXXd>(spots.data(), nRows, 1);
 }
