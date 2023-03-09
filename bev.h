8f4e16fc49c7d6fb760e0643021a509a139d7bc7
Nic Mitoulis <120728902+nmitou@users.noreply.github.com>
2023-03-09 16:08:44 +0200

    rough outline of bev class

---
+++ bev.h
@@ -0,0 +1,23 @@
+#ifndef BEV_H
+#define BEV_H
+
+namespace bev {
+	class BEV {
+		// data
+		// interest rate r, strike or strike list, term/maturity or maturities
+	public:
+		// BEV() // constructor
+		// BEV( data ); // constructor with data/path
+		// BEV(data, r, strike, maturity) // if looking for one bev result
+		// BEV(data, r, strikes list, maturity) // if looking for a skew
+		// BEV(data, r, strikes list, maturities list) // for surface
+
+		// void SetData(); 
+		// void SetDateRange()
+
+		
+
+	}
+}
+
+#endif
