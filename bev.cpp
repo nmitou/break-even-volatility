d556516affcdf4f1e2c39d5e3e92f6f76fc15b24
Nic Mitoulis <120728902+nmitou@users.noreply.github.com>
2023-03-14 12:24:14 +0200

    constructors and getters/setters

--- bev.cpp
+++ bev.cpp
@@ -1,13 +1,49 @@
 #include "bev.h"
+#include "utils.h"
 #include <Eigen/Dense>
+#include <string>
 
 using namespace bev;
 
-/*	Creates an array of subpaths where each row is a sub-sample/path of length 21 (days) * term_in_months
-	and the number of rows is dependent on the size of the sample data and the corresponding term 
-	parameter entered. */
-Eigen::ArrayXXd BEV::GetSubPaths(Eigen::ArrayXXd& path, int term_in_months) {
+// CONSTRUCTORS
+/*
+Construct with filepath to CSV data. */
+BEV::BEV(std::string csv_path) 
+	: path_(data_utils::CSVToEigenArray(csv_path)) {}// path_ = data_utils::CSVToEigenArray(csv_path);}
+/*	
+Another constructor. Usage: one can input one-element vectors (for strikes, maturities) which will give 
+a single BEV result, or multiple strikes and a one-element maturity vector for a single skew, or 
+multiple strikes and maturities for a surface. */
+BEV::BEV(double interest_rate, std::vector strikes, std::vector maturities) 
+	: interest_rate_(interest_rate)
+	, strikes_(strikes)
+	, maturities_(maturities)
+	{}
+/*
+Constructor with all private variables set. See comment above. */
+BEV::BEV(std::string csv_path, double interest_rate, std::vector strikes, std::vector maturities)
+	: path_(data_utils::CSVToEigenArray(csv_path))
+	, interest_rate_(interest_rate)
+	, strikes_(strikes)
+	, maturities_(maturities)
+	{}
+
+// SETTERS
+void BEV::SetData(std::string csv_path) { path_ = data_utils::CSVToEigenArray(csv_path); }
+
+// SOLVING FOR BEV
+
+double BEV::ContinuousDHPnL(double sigma, Eigen::ArrayXXd& paths, double strike, double maturity) {
+	// test whether paths has single row or column
+
+}
+
+/*	
+Creates an array of subpaths where each row is a sub-sample/path of length 21 (days) * term_in_months
+and the number of rows is dependent on the size of the sample data and the corresponding term 
+parameter entered. */
+Eigen::ArrayXXd BEV::GetSubPaths(int term_in_months) {
 	int days_to_maturity = term_in_months*21;
-	int n_sub_paths = path.size() / (days_to_maturity); // integer division => removes remaining datapoints which are less than full sub path
-	return path(seq(0, n_sub_paths*days_to_maturity - 1), all).reshaped<Eigen::RowMajor>(n_sub_paths, days_to_maturity);
+	int n_sub_paths = path_.size() / (days_to_maturity); // integer division => removes remaining datapoints which are less than a full sub path
+	return path_(seq(0, n_sub_paths*days_to_maturity - 1), all).reshaped<Eigen::RowMajor>(n_sub_paths, days_to_maturity);
 }
--- bev.h
+++ bev.h
@@ -20,35 +20,46 @@ namespace bev {
 
 	public:
 		BEV() {}; // default constructor
-		BEV(std::string csv_path); // constructor with filepath to csv data
-		BEV(std::string csv_path, double interest_rate, std::vector strikes, std::vector maturities); // for surface, can put one element vectors for single bev result or 1-elm maturity vec for single skew
+		// constructor with filepath to CSV data
+		BEV(std::string csv_path); 
+		/*	
+		Another constructor. Usage: one can input one-element vectors (for strikes, maturities) 
+		which will give a single BEV result, or multiple strikes and a one-element maturity vector
+		for a single skew, or multiple strikes and maturities for a surface. */
+		BEV(double interest_rate, std::vector strikes, std::vector maturities);
+		// Constructor with all private variables set. See comment above.
+		BEV(std::string csv_path, double interest_rate, std::vector strikes, std::vector maturities);
 
 		// Setters:
-		void SetData(); 
+		void SetData(std::string csv_path);
+		void SetInterestRate(double interest_rate) { interest_rate_ = interest_rate; }
+		void SetStrikes(std::vector strikes) { strikes_ = strikes; }
+		void SetMaturities(std::vector maturities) { maturities_ = maturities; }
 		// void SetDateRange(); // for future
-		void SetDataIndexRange();
-		void SetInterestRate();
-		void SetStrikes();
-		void SetMaturities();
+		// void SetDataIndexRange();
 		// Getters:
-		/// !!!
+		double GetInterestRate() { return interest_rate_; }
+		std::vector GetStrikes() { return strikes_; }
+		std::vector GetMaturities { return maturities_; }
 
-		// for solving for bev:
+		// Solving for BEV:
+
+		Eigen::ArrayXXd SolveForBEV(); // average pnls? flag; check best way for flag variable in c++
 
 		// double DailyDHPnL(double sigma, !!! other params); 
-		// double ContinuousDHPnL(double sigma, !!! other params); 
+		double ContinuousDHPnL(double sigma, Eigen::ArrayXXd& paths, double strike, double maturity); 
 
 		/*	
 		Creates an array of subpaths (rows) of length 21 (days) * term_in_months, with the number of rows 
 		depending on the size of the sample data and the corresponding term	parameter entered. */
-		Eigen::ArrayXXd GetSubPaths(Eigen::ArrayXXd& path, int term_in_months);
+		Eigen::ArrayXXd GetSubPaths(int term_in_months);
 
 		// make functions work for single path of matrix/array of paths (decide which dimension for time and path number)
 		// use lambda function of sigma when using this in pnl function
 		// use reference for paths
 		Eigen::ArrayXXd BlackScholesGamma(Eigen::ArrayXXd paths, double strike, double maturity, double interest_rate, double sigma);
 
-		Eigen::ArrayXXd SolveForBEV(); // average pnls? flag; check best way for flag variable in c++
+		
 		
 
 	}
