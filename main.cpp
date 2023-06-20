16f8bd5973d691d1bb9cf2c9b2fba425d08cdfaf
Nic Mitoulis <120728902+nmitou@users.noreply.github.com>
2023-06-20 13:12:41 +0200

    main example with google data

--- main.cpp
+++ main.cpp
@@ -1,74 +1,53 @@
 /*
+	Calculating the break-even volatility surface for Google/Alphabet's share price from 2018-2023.
 
-compile with g++ -I C:\local\eigen-3.4.0 -o bev_class_testing utils.o bev.o bev_class_testing.cpp 
+	Compile with:
+		g++ -I path/to/eigen -I path/to/bev -I path/to/utils -o main main.cpp bev.cpp utils.cpp
+	or with cmake.
 */
 #include <iostream>
-#include "bev.h"
 #include <vector>
 #include <Eigen/Dense>
+#include "bev.h"
+#include "utils.h"
+
+using bev_utils::PrintResults;
 
 int main() {
-	double r = 0.065;
+	// Choose interest rate to apply over period, let's say an average interest of 1.5% applied over the period in the US
+	double r = 0.015;
+	// Strikes in relative (%) terms as stock price will be rebased in multiple sub-periods, in order to aggregate BEV results
 	std::vector<double> strikes = {0.80, 0.85, 0.90, 0.95, 1.00, 1.05, 1.10, 1.15, 1.20};
+	// Terms/maturities in months
 	std::vector<int> maturities = {1, 2, 3, 4, 5, 6, 9, 12, 15, 18};
-	bev::BEV bev_obj("sampledata.csv", r, strikes, maturities, 0);
+	// Create BEV object with data from CSV, specifying we want the Close prices
+	bev::BEV GOOG_bev("GOOG.csv", r, strikes, maturities, -1, true, "Close");
+	// Compute the volatility surface based on the data entered, this will solve for the break-even volatility which
+	// zeroes the average profit/loss (average of sub-periods PnL's) for each maturity-strike combination
+	Eigen::ArrayXXd GOOG_surface = GOOG_bev.SolveForBEV();
+	std::cout << "\nBEV surface with average PnL zeroed:" << std::endl << std::endl;
+	PrintResults(strikes, maturities, GOOG_surface);
 
-	Eigen::ArrayXXd bev_output = bev_obj.SolveForBEV();
+	// We can repeat the procedure, choosing the average the break-even volatilities found from each period instead
+	GOOG_surface = GOOG_bev.SolveForBEV(false);
+	std::cout << "\nBEV surface with BEVs averaged:" << std::endl << std::endl;
+	PrintResults(strikes, maturities, GOOG_surface);
 
-	std::cout << "\nBEV array (average pnl zeroed) =\n" << std::endl << bev_output << std::endl;
+	std::cout << "\nNotice the vast differences in shorter-dated skews between the two aggregation methods." << std::endl;
 
-	bev_output = bev_obj.SolveForBEV(false);	
+	// If we only want one strike maturity combination
+	double strike = 1.05;
+	int maturity = 3;
+	GOOG_bev.SetStrikes(std::vector<double> {strike});
+	GOOG_bev.SetMaturities(std::vector<int> {maturity});
+	Eigen::ArrayXXd res = GOOG_bev.SolveForBEV(false); // once again, one can choose whether to average the PnL's or not
+	std::cout << "\nBEV result for strike = " << strike << " and maturity = " << 3 << " months: " << res << std::endl << std::endl;
 
-	std::cout << "\nBEV array (average path BEV) =\n" << std::endl << bev_output << std::endl;
-
-	// sometimes gives nans, so lets investigate
-	std::cout << "\nLets investigate the nan's:\n" << std::endl;
-
-	std::cout << "Break-even volatility for strike of 100% and 1 month term:" << std::endl;
-	std::cout << bev_output(0, 4) << " <- this is fine" << std::endl << std::endl;
-
-	double strike = 0.80;
-	int maturity = 1;
-
-	bev_obj.SetStrikes(std::vector<double> {strike});
-	bev_obj.SetMaturities(std::vector<int> {maturity});
-
-	Eigen::ArrayXXd bev_80K_1M = bev_obj.SolveForBEV(false);
-
-	std::cout << "Break-even volatility for strike of " << strike << " and " << maturity << " month term:" << std::endl;
-	std::cout << "bev_80K_1M = " << bev_80K_1M << std::endl << std::endl;
-
-	Eigen::ArrayXXd path_bevs = bev_obj.SolveForBEV(strike, maturity);
-
-	std::cout << "BEV (strike=" << strike << ", term=" << maturity << " month) for each subpath:" << std::endl;
-	std::cout << "path_bevs =\n" << path_bevs.transpose() << std::endl;	
-	// nan results for some paths
-	// specifically, the 11th result
-	Eigen::ArrayXXd paths = bev_obj.GetSubPaths(maturity);
-	std::cout << "BEV result for 11th path = " << path_bevs(10, 0) << std::endl;
-	std::cout << "11th path =\n" << paths(10, Eigen::all) << std::endl << std::endl;
-
-	std::cout << "Let's double check the BEVs for each path for a strike, maturity combination that doesn't produce nan:\n" <<
-					"Path BEVs for example above (strike=100%, term=1 month) =\n" << bev_obj.SolveForBEV(1.0, 1).transpose() << std::endl <<
-					"... no nan's to see here." << std::endl << std::endl;
-
-	std::cout << "Back to the result for the 11th path:\n" << 
-					"Let's check the PnL for a range of volatilities:" << std::endl;
-	// need times to maturity vector/array for pnl calculation
-	int days_to_maturity = maturity*21;
-	Eigen::Array<double, 1, Eigen::Dynamic> times_to_maturity = Eigen::ArrayXd::LinSpaced(days_to_maturity, (double) days_to_maturity-1, 0.0) * (1.0 / 252.0);
-	// linspace variables for sigma range
-	int n = 20; // number of steps in sigma range
-	double a = 0.01, b = 0.21; // sigma range start/end
-	std::cout << "PnL(sigma) = ..." << std::endl;
-	for (int i=0; i<n; i++) {
-		double sigma = a + i*((b-a)/n);
-		std::cout << "PnL(" << sigma << ") = " << bev_obj.ContinuousDHPnL(sigma, paths(10, Eigen::all), strike, times_to_maturity) << std::endl;
-	}
-
-	// also test k=105, term=9m
-
-
+	// If we want all the break-even volatilities from each of the sub-periods included
+	Eigen::ArrayXXd path_bevs = GOOG_bev.SolveForBEV(strike, maturity);
+	std::cout << "Here are the break-even volatility results for strike = " << strike << " and for each " << maturity << " month period in the data:" << std::endl << std::endl;
+	std::cout << path_bevs << std::endl << std::endl;
+	std::cout << "Which average to " << path_bevs.mean() << " as above." << std::endl;
 
 	return 0;
 }
